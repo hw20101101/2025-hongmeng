55f1a8f9abca5e7b31ad6c409ceaecd040b49ba4
hwacdx <1296324895@qq.com>
2025-05-23 00:10:56 +0800

    熟悉 ArkTS工程目录结构（Stage模型）

--- entry/hvigorfile.ts
+++ entry/hvigorfile.ts
@@ -1,5 +1,6 @@
 import { hapTasks } from '@ohos/hvigor-ohos-plugin';
 
+//注：模块级编译构建任务脚本
 export default {
     system: hapTasks,  /* Built-in plugin of Hvigor. It cannot be modified. */
     plugins:[]         /* Custom plugin to extend the functionality of Hvigor. */
--- hvigorfile.ts
+++ hvigorfile.ts
@@ -1,5 +1,6 @@
 import { appTasks } from '@ohos/hvigor-ohos-plugin';
 
+//注：工程级编译构建任务脚本
 export default {
     system: appTasks,  /* Built-in plugin of Hvigor. It cannot be modified. */
     plugins:[]         /* Custom plugin to extend the functionality of Hvigor. */
